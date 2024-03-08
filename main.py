38da6abba26671b3ac7119276f31ee758189051d
Kristoffer Stensbo-Smidt <kss@prior.info>
2024-03-08 21:12:59 +0100

    Feature: sample multiple CDFs.

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
 from jax import random
+from matplotlib import cm
+
+from IPython import embed  # noqa: F401
 
 jax.config.update("jax_enable_x64", True)
 # jax.config.update("jax_platform_name", "cpu")
@@ -17,28 +20,34 @@ def main() -> None:
     seed = 0
     key = random.PRNGKey(seed)
 
-    Kmax = 5
-    alpha = 1
+    num_cdfs = 100
+
+    Kmax = 50
+    alpha = 10
 
     key, beta_key = random.split(key)
+    betas = random.beta(beta_key, 1, alpha, shape=[num_cdfs, Kmax])
+    betas = betas.at[:, -1].set(1)
 
-    betas = random.beta(beta_key, 1, alpha, shape=[Kmax])
-    betas = betas.at[-1].set(1)
-
-    beta_prod = jnp.cumprod(1 - betas)
+    beta_prod = jnp.cumprod(1 - betas, axis=1)
     weights = betas
-    weights = weights.at[1:].set(betas[1:] * beta_prod[:-1])
+    weights = weights.at[:, 1:].set(betas[:, 1:] * beta_prod[:, :-1])
 
     key, normal_key = random.split(key)
-    deltas = random.normal(normal_key, shape=[Kmax])
+    deltas = random.normal(normal_key, shape=[num_cdfs, Kmax])
 
-    deltas = np.array(deltas)
-    weights = np.array(weights)
+    deltas = np.asarray(deltas)
+    weights = np.asarray(weights)
+
+    print(f"Integral of CDFs: {np.sum(weights, axis=1)}")
 
     _, ax = plt.subplots()
 
-    ax.ecdf(deltas, weights, color="C1")
-    ax.vlines(deltas, 0, weights, color="C0")
+    for n, (d, w) in enumerate(zip(deltas, weights, strict=False)):
+        ax.ecdf(d, w, color=cm.Oranges(0.2 + 0.6 * (n / (num_cdfs - 1))), alpha=0.7)
+        ax.vlines(d, 0, w, color=cm.Blues(0.2 + 0.6 * (n / (num_cdfs - 1))), alpha=0.7)
+    # ax.ecdf(deltas, weights, color="C1")
+    # ax.vlines(deltas, 0, weights, color="C0")
 
     ax.set_ylim(bottom=0)
 
